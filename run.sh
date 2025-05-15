80b66110b1a4a3e84bedc044954427415ac6a678
zeauw <me@zeauw.dev>
2025-05-15 15:12:17 +0800

    fix: fix listen addr

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ if [ -Z $(ls -A /wiki) ]; then
     tiddlywiki /wiki --init server
 fi
 
-tiddlywiki /wiki --listen username=$TIDDLYWIKI_USERNAME password=$TIDDLYWIKI_PASSWORD
+tiddlywiki /wiki --listen host=0.0.0.0 username=$TIDDLYWIKI_USERNAME password=$TIDDLYWIKI_PASSWORD

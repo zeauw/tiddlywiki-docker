4077c1283a6ba63cbd75a218365147274d5cd245
zeauw <me@zeauw.dev>
2025-07-24 20:14:00 +0800

    fix: fix timed out when stopping container

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ if [ ! -e /wiki/tiddlywiki.info ]; then
 fi
 
 if [[ -n "$TIDDLYWIKI_PASSWORD"  &&  -n "$TIDDLYWIKI_USERNAME" ]]; then
-    tiddlywiki /wiki --listen host=0.0.0.0 username=$TIDDLYWIKI_USERNAME password=$TIDDLYWIKI_PASSWORD
+    exec tiddlywiki /wiki --listen host=0.0.0.0 username=$TIDDLYWIKI_USERNAME password=$TIDDLYWIKI_PASSWORD
 else
-    tiddlywiki /wiki --listen host=0.0.0.0
+    exec tiddlywiki /wiki --listen host=0.0.0.0
 fi
